ce461757f8caf1c8f6fdf1c189f742260ec6dc3f
youzhan <751642772@qq.com>
2021-09-19 18:59:38 +0800

    9.19

---
+++ src/main/java/com/itmuch/contentcenter/domain/dto/content/ShareAuditDTO.java
@@ -0,0 +1,16 @@
+package com.itmuch.contentcenter.domain.dto.content;
+
+import com.itmuch.contentcenter.domain.enums.AuditStatusEnum;
+import lombok.Data;
+
+@Data
+public class ShareAuditDTO {
+    /**
+     * 审核状态
+     */
+    private AuditStatusEnum auditStatusEnum;
+    /**
+     * 原因
+     */
+    private String reason;
+}
--- src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
+++ src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
@@ -3,14 +3,18 @@ package com.itmuch.contentcenter.service.content;
 
 import com.itmuch.contentcenter.dao.content.ShareMapper;
 
+import com.itmuch.contentcenter.domain.dto.content.ShareAuditDTO;
 import com.itmuch.contentcenter.domain.dto.content.ShareDTO;
 
+import com.itmuch.contentcenter.domain.dto.messaging.UserAddBonusMsgDTO;
 import com.itmuch.contentcenter.domain.dto.user.UserDTO;
 import com.itmuch.contentcenter.domain.entity.content.Share;
 
+import com.itmuch.contentcenter.domain.enums.AuditStatusEnum;
 import com.itmuch.contentcenter.feignclient.UserCenterFeignClient;
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,6 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
@@ -36,6 +41,8 @@ public class ShareService {
     private DiscoveryClient discoveryClient;
     @Autowired
     private UserCenterFeignClient userCenterFeignClient;
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
     public ShareDTO findById(Integer id) {
         // 获取分享详情
         Share share = this.shareMapper.selectByPrimaryKey(id);
@@ -49,6 +56,27 @@ public class ShareService {
         BeanUtils.copyProperties(share, shareDTO);
         shareDTO.setWxNickname(userDTO.getWxNickname());
         return shareDTO;
+    }
+    public Share auditById(Integer id, ShareAuditDTO auditDTO) {
+        // 1. 查询share是否存在，不存在或者当前的audit_status != NOT_YET，那么抛异常
+        Share share = this.shareMapper.selectByPrimaryKey(id);
+        if (share == null) {
+            throw new IllegalArgumentException("参数非法！该分享不存在！");
+        }
+        if (!Objects.equals("NOT_YET", share.getAuditStatus())) {
+            throw new IllegalArgumentException("参数非法！该分享已审核通过或审核不通过！");
+        }
+        share.setAuditStatus(auditDTO.getAuditStatusEnum().toString());
+        shareMapper.updateByPrimaryKeySelective(share);
+        // 3. 如果是PASS，那么发送消息给rocketmq，让用户中心去消费，并为发布人添加积分
+        if (AuditStatusEnum.PASS.equals(auditDTO.getAuditStatusEnum())) {
+            rocketMQTemplate.convertAndSend("add-bonus",
+                    UserAddBonusMsgDTO.builder()
+                    .userId(share.getUserId())
+                    .bonus(50)
+                    .build());
+        }
+        return share;
     }
 }
 
